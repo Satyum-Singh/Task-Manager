67fa2ddc330c7326692dd8e3acdf7f3cca9b9363
Satyum <satyumspathania@gmail.com>
2024-07-13 21:20:40 +0530

    working on ng8002.

--- src/app/Mycomponents/todos/todos.component.ts
+++ src/app/Mycomponents/todos/todos.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import {Todo} from '../../Todo';
+import { NgFor } from '@angular/common';
+import { TodoItemComponent } from '../todo-item/todo-item.component';
+
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [],
+  imports: [NgFor,TodoItemComponent],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.css'
 })
 export class TodosComponent {
-
+  todos : Todo[];
+  constructor() {
+    this.todos = [
+      {
+        sno:1,
+        title:"This is title 1",
+        desc:"Description 1",
+        active:true
+      },
+      {
+        sno:2,
+        title:"This is title 2",
+        desc:"Description 2",
+        active:true
+      },
+      {
+        sno:3,
+        title:"This is title 3",
+        desc:"Description 3" ,
+        active:true
+      }
+    ]
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {TodosComponent} from './Mycomponents/todos/todos.component'
-
+import {TodoItemComponent} from './Mycomponents/todo-item/todo-item.component'
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,TodosComponent],
+  imports: [RouterOutlet,TodosComponent,CommonModule,TodoItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
